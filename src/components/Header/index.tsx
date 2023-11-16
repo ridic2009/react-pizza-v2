d43a4da7bdbac825b7223cb1c04136fd90214475
daniels <daniel.shchepin@gmail.com>
2023-11-16 18:49:09 +0700

    чё то ебать добавил раскумар

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import pizzaLogo from "../../assets/img/pizza-logo.svg";
+// import pizzaLogo from "../../assets/img/PizzaLogo.png";
 import Search from "../Search";
 
 import { Link } from "react-router-dom";
@@ -17,7 +17,7 @@ export default function Header() {
       <div className="container">
         <Link to="/">
           <div className="header__logo">
-            <img width="38" src={pizzaLogo} alt="Pizza logo" />
+            <img width="38" src={'#'} alt="Pizza logo" />
             <div>
               <h1>React Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 // React
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
